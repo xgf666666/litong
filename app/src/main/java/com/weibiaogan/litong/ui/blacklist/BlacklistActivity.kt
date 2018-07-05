23f2b1b44a4654ec85a9c628793b233642ced071
GUBR\Gubr <Gubr@foxmail.com>
2018-07-05 20:27:26 +0800

    添加界面

---
+++ app/src/main/java/com/weibiaogan/litong/ui/blacklist/BlacklistActivity.kt
@@ -0,0 +1,130 @@
+package com.weibiaogan.litong.ui.blacklist
+
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.weibiaogan.litong.R
+import com.weibiaogan.litong.extensions.loadImag
+import com.weibiaogan.litong.mvp.contract.BlacklistContract
+import com.weibiaogan.litong.mvp.presenter.BlacklistPresenter
+import com.xx.baseuilibrary.mvp.lcec.BaseMvpLcecActivity
+import kotlinx.android.synthetic.main.activity_blacklist.*
+
+/**
+ * author: Gubr
+ * date: 2018/7/5
+ * describe:
+ */
+
+class BlacklistActivity:BaseMvpLcecActivity<RelativeLayout,List<Any>,BlacklistContract.Model,BlacklistContract.View,BlacklistPresenter>(){
+    override fun loadData(refresh: Boolean) {
+
+    }
+
+    override fun setData(data: List<Any>?) {
+    }
+
+    override fun createPresenter(): BlacklistPresenter {
+        return BlacklistPresenter()
+    }
+
+    override fun getActivityLayoutId(): Int {
+        return R.layout.activity_blacklist
+    }
+
+    override fun initEvent() {
+    }
+//    override fun loadData(refresh: Boolean) {
+//        presenter.getData(true,0)
+//    }
+//
+//
+//    private val adapter by lazy {
+//        BuybackAdapter(null)
+//    }
+//
+//
+//    override fun createPresenter(): BlacklistPresenter {
+//        return BlacklistPresenter()
+//    }
+//
+//    override fun getActivityLayoutId(): Int {
+//        return R.layout.activity_blacklist
+//    }
+//
+//    override fun initData() {
+//        super.initData()
+//        initrecyclerView()
+//        getPresenter().getData(true, 0)
+//    }
+//
+//    override fun initEvent() {
+//
+//        view_content.setOnRefreshListener { getPresenter().getData(true, 0) }
+//
+//    }
+//
+//    private fun initrecyclerView() {
+//
+//
+//        adapter.setOnItemClickListener { _, view, position ->
+//            val item = adapter.getItem(position)
+//
+//        }
+//
+//        adapter.setOnItemChildClickListener { _, view, position ->
+//
+//
+//        }
+//        adapter.setOnLoadMoreListener({ getPresenter().loadData() }, recyclerView)
+//
+//
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerView.adapter = adapter
+//
+//    }
+//
+//
+//    override fun setData(data: List<Any>?) {
+//
+//        adapter.setNewData(data)
+//        showContent()
+//        view_content.isRefreshing = false
+//        if (data?.size?:0==0){
+//            adapter.setEmptyView(R.layout.item_view_empty,recyclerView)
+//        }
+//        if (data?.size ?: 0 >= 10) {
+//            adapter.setEnableLoadMore(true)
+//        } else {
+//            adapter.loadMoreEnd()
+//            adapter.setEnableLoadMore(false)
+//        }
+//
+//
+//    }
+//
+//    override fun addData(data: List<Any>?) {
+//
+//        if (data != null) {
+//            adapter.addData(data)
+//        }
+//        if (data?.size ?: 0 >= 10) {
+//            adapter.loadMoreComplete()
+//        } else {
+//            adapter.loadMoreEnd()
+//        }
+//    }
+
+
+
+
+    class BuybackAdapter(data: MutableList<Any>?) : BaseQuickAdapter<Any, BaseViewHolder>(R.layout.item_backlist, data) {
+        override fun convert(helper: BaseViewHolder?, item: Any) {
+
+        }
+    }
+
+
+}
