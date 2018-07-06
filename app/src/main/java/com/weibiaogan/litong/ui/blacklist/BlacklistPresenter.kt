7a43fb4d6b805ced527783e967a1ee63c3e858dd
GUBR\Gubr <Gubr@foxmail.com>
2018-07-06 10:35:04 +0800

    添加用户模块-黑名单列表

--- app/src/main/java/com/weibiaogan/litong/ui/blacklist/BlacklistPresenter.kt
+++ app/src/main/java/com/weibiaogan/litong/ui/blacklist/BlacklistPresenter.kt
@@ -1,5 +1,7 @@
 package com.weibiaogan.litong.ui.blacklist
 
+import com.weibiaogan.litong.common.Constants
+import com.weibiaogan.litong.extensions.loadDefulat
 import com.weibiaogan.litong.extensions.ui
 import com.weibiaogan.litong.ui.blacklist.BlacklistContract
 import com.weibiaogan.litong.ui.blacklist.BlacklistModel
@@ -10,9 +12,44 @@ import com.weibiaogan.litong.ui.blacklist.BlacklistModel
  * describe:黑名单列表
  */
 class BlacklistPresenter :  BlacklistContract.Presenter() {
-    override fun getData() {
-        getModel().getData().ui({},{})
+
+
+
+
+    private var page = 0
+
+    override fun getData(isRefresh: Boolean, page: Int) {
+        if (Constants.isLogin()) {
+
+
+            val userId = Constants.getToken().user_id.toString()
+            val token = Constants.getToken().token
+            this.page = page
+            getModel().getData(userId, token, "$page")
+                    .apply {
+                        if (isRefresh) loadDefulat(getView()!!)
+                    }
+                    .ui({
+                        var data = it.data
+                        if (isRefresh) {
+                            getView()?.setData(data)
+                        } else {
+                            getView()?.addData(data)
+                        }
+                    }, {
+                        getView()?.showToast(it)
+                    })
+        }else{
+            getView()?.showToast("请先登录")
+        }
     }
+
+
+    override fun loadData() {
+        getData(false, ++page)
+    }
+
+
 
     override fun createModel(): BlacklistContract.Model {
         return BlacklistModel()
